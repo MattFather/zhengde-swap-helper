d603a03322f14e1e8740a808a1685663598a5582
MattFather <ed0224@apps.ntpc.edu.tw>
2026-05-02 19:19:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -551,7 +551,7 @@ def render_jump_button():
 st.markdown("""
 <style>
 .my-super-title {
-    font-size: 28px !important;
+    font-size: 60px !important;
     font-weight: 900 !important;
     line-height: 1.3 !important;
     margin-bottom: 15px !important;
